49f6dee300ba6397a1b7d39eccf51015ac894b33
arti0108 <gaonkararti@gmail.com>
2017-05-10 10:50:49 +0530

    hello
    Signed-off-by: arti0108 <gaonkararti@gmail.com>

---
+++ src/lab1537/lightbulb.java
@@ -0,0 +1,17 @@
+package lab1537;
+
+public class lightbulb {
+
+	private int lightbulb;
+
+	public lightbulb(int watt)
+	{
+	this.lightbulb=watt;
+	}
+
+	public int getLightbulb() {
+		return lightbulb;
+	}
+
+	
+}
---
+++ src/lab1537/lightbulbTest.java
@@ -0,0 +1,22 @@
+package lab1537;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class lightbulbTest {
+	
+	lightbulb b;
+
+	@Before
+	public void setUp() throws Exception {
+		b=new lightbulb(200);
+	}
+
+	@Test
+	public void testGetLightbulb() {
+		assertEquals(b.getLightbulb(),200);
+	}
+
+}
